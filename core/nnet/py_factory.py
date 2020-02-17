8b1fdfeef36e0a1beebec9486a828666b7d1aae7
Tien <viettienha98@gmail.com>
2020-02-17 15:45:56 +0700

    1

--- core/nnet/py_factory.py
+++ core/nnet/py_factory.py
@@ -83,9 +83,11 @@ class NetworkFactory(object):
         self.model.cuda()
 
     def train_mode(self):
+        for param in self.model.module.parameters():
+            param.requires_grad = True
         self.network.train()
-        if self.freeze:
-            self.learn_transfer()
+        # if self.freeze:
+        #     self.learn_transfer()
 
     def eval_mode(self):
         for param in self.model.module.parameters():
